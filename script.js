003297a003897833197384a703696ac1de3b14f0
Dingonils <64286206+Dingonilss@users.noreply.github.com>
2024-10-12 10:59:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -155,8 +155,8 @@ async function checkManuellWebsiteAndRedirect(url) {
 
 function verbindeMitServer(adress = urlToCheck) { // eslint-disable-line no-unused-vars
     automatischeSucheAbbrechen = true
-    wbsIframe.src = adress
-    wbsIframe.style.display = 'block'
-    document.getElementsByTagName('main')[0].innerHTML = ''
-    //window.location.href = urlToCheck
+    //wbsIframe.src = adress
+    //wbsIframe.style.display = 'block'
+    //document.getElementsByTagName('main')[0].innerHTML = ''
+    window.location.href = urlToCheck
 }
